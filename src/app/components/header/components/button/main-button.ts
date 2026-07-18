f954fdf3c0baab20e73f4af15a9f2f17373071ee
Juan Buri <juancalveti4@gmail.com>
2026-07-17 22:18:54 -0500

    feat(header/components): create Logo and MainButton standalone subcomponents

---
+++ src/app/components/header/components/button/main-button.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-main-button',
+    standalone: true,
+    imports: [],
+    templateUrl: 'main-button.html',
+    styleUrl: 'main-button.css',
+})
+
+export class MainButton { }
